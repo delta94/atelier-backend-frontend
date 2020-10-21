49e4dd83af188b2d10a70ca0511bc523c41583ff
divakarr2017 <divakarr@simsaw.com>
2020-10-22 00:06:30 +0530

    local changes

--- src/ApiActions/DbConfig/ApiBaseUrl.js
+++ src/ApiActions/DbConfig/ApiBaseUrl.js
@@ -1,6 +1,7 @@
-export const apiBaseUrl = "https://ateli.herokuapp.com/api";
+//export const apiBaseUrl = "https://ateli.herokuapp.com/api";
 
-// export const apiBaseUrl="http://192.168.1.9:8081";
+export const apiBaseUrl="http://192.168.1.9:8081";
+
 export const apiCommonParams = {
     'CLIENT_ID': 'RefreshAesthetic',
     'CLIENT_SECRET': 'refreshAesthetic!46#',
